cee6f9ec1b18661a580386c411827e097a2f8e7e
Isaac Katana <109860816+isaackatana@users.noreply.github.com>
2023-01-06 14:49:59 +0300

    #

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+// menu bar .............................
+
 var bar = document.getElementById('bar');
 var close = document.getElementById('close');
 var nav = document.getElementById('navbar');
@@ -12,4 +15,23 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active')
     })
+}
+
+// contact form .............................
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "mrisaackatana@gmail.com",
+        Password : "StillScorpio101",
+        To : 'mrisaackatana@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiries",
+        Body : "Name: " + document.getElementById("name").value
+        + "<br> Email : " + document.getElementById("email").value
+        + "<br> Phone no : " + document.getElementById("phone").value
+        + "<br> Message : " + document.getElementById("message").value
+    }).then(
+        message => alert('Message sent successfully')
+    );
 }
